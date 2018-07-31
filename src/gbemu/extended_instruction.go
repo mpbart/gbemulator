1371c779c3b6d288fcbac8ed1f49e3a0dad28df4
Michael Barton <michael.barton@avant.com>
2018-07-31 16:32:18 -0500

    Other implied extended instructions added

--- src/gbemu/extended_instruction.go
+++ src/gbemu/extended_instruction.go
@@ -137,30 +137,34 @@ type bitFromMemoryInstruction struct {
 
 type setInstruction struct {
 	basicInstruction
-	source Register
-	regs   Registers
+	bitNumber uint
+	source    Register
+	regs      Registers
 }
 
 type setFromMemoryInstruction struct {
 	basicInstruction
-	source1 Register
-	source2 Register
-	regs    Registers
-	mmu     MMU
+	bitNumber uint
+	source1   Register
+	source2   Register
+	regs      Registers
+	mmu       MMU
 }
 
 type resetInstruction struct {
 	basicInstruction
-	source Register
-	regs   Registers
+	bitNumber uint
+	source    Register
+	regs      Registers
 }
 
 type resetFromMemoryInstruction struct {
 	basicInstruction
-	source1 Register
-	source2 Register
-	regs    Registers
-	mmu     MMU
+	bitNumber uint
+	source1   Register
+	source2   Register
+	regs      Registers
+	mmu       MMU
 }
 
 func CreateExtendedInstructions(regs Registers, mmu MMU) map[byte]ExtendedInstruction {
@@ -293,22 +297,134 @@ func CreateExtendedInstructions(regs Registers, mmu MMU) map[byte]ExtendedInstru
 		0x7D: &bitInstruction{basicInstruction{8, 0}, 7, l, regs},
 		0x7E: &bitFromMemoryInstruction{basicInstruction{16, 0}, 7, h, l, regs, mmu},
 		0x7F: &bitInstruction{basicInstruction{8, 0}, 7, a, regs},
-		0xC0: &setInstruction{basicInstruction{8, 0}, b, regs},
-		0xC1: &setInstruction{basicInstruction{8, 0}, c, regs},
-		0xC2: &setInstruction{basicInstruction{8, 0}, d, regs},
-		0xC3: &setInstruction{basicInstruction{8, 0}, e, regs},
-		0xC4: &setInstruction{basicInstruction{8, 0}, h, regs},
-		0xC5: &setInstruction{basicInstruction{8, 0}, l, regs},
-		0xC6: &setFromMemoryInstruction{basicInstruction{16, 1}, h, l, regs, mmu},
-		0xC7: &setInstruction{basicInstruction{8, 0}, a, regs},
-		0x80: &resetInstruction{basicInstruction{8, 0}, b, regs},
-		0x81: &resetInstruction{basicInstruction{8, 0}, c, regs},
-		0x82: &resetInstruction{basicInstruction{8, 0}, d, regs},
-		0x83: &resetInstruction{basicInstruction{8, 0}, e, regs},
-		0x84: &resetInstruction{basicInstruction{8, 0}, h, regs},
-		0x85: &resetInstruction{basicInstruction{8, 0}, l, regs},
-		0x86: &resetFromMemoryInstruction{basicInstruction{16, 1}, h, l, regs, mmu},
-		0x87: &resetInstruction{basicInstruction{8, 1}, a, regs},
+		0x80: &resetInstruction{basicInstruction{8, 0}, 0, b, regs},
+		0x81: &resetInstruction{basicInstruction{8, 0}, 0, c, regs},
+		0x82: &resetInstruction{basicInstruction{8, 0}, 0, d, regs},
+		0x83: &resetInstruction{basicInstruction{8, 0}, 0, e, regs},
+		0x84: &resetInstruction{basicInstruction{8, 0}, 0, h, regs},
+		0x85: &resetInstruction{basicInstruction{8, 0}, 0, l, regs},
+		0x86: &resetFromMemoryInstruction{basicInstruction{16, 0}, 0, h, l, regs, mmu},
+		0x87: &resetInstruction{basicInstruction{8, 1}, 0, a, regs},
+		0x88: &resetInstruction{basicInstruction{8, 0}, 1, b, regs},
+		0x89: &resetInstruction{basicInstruction{8, 0}, 1, c, regs},
+		0x8A: &resetInstruction{basicInstruction{8, 0}, 1, d, regs},
+		0x8B: &resetInstruction{basicInstruction{8, 0}, 1, e, regs},
+		0x8C: &resetInstruction{basicInstruction{8, 0}, 1, h, regs},
+		0x8D: &resetInstruction{basicInstruction{8, 0}, 1, l, regs},
+		0x8E: &resetFromMemoryInstruction{basicInstruction{16, 0}, 1, h, l, regs, mmu},
+		0x8F: &resetInstruction{basicInstruction{8, 0}, 1, a, regs},
+		0x90: &resetInstruction{basicInstruction{8, 0}, 2, b, regs},
+		0x91: &resetInstruction{basicInstruction{8, 0}, 2, c, regs},
+		0x92: &resetInstruction{basicInstruction{8, 0}, 2, d, regs},
+		0x93: &resetInstruction{basicInstruction{8, 0}, 2, e, regs},
+		0x94: &resetInstruction{basicInstruction{8, 0}, 2, h, regs},
+		0x95: &resetInstruction{basicInstruction{8, 0}, 2, l, regs},
+		0x96: &resetFromMemoryInstruction{basicInstruction{16, 0}, 2, h, l, regs, mmu},
+		0x97: &resetInstruction{basicInstruction{8, 1}, 2, a, regs},
+		0x98: &resetInstruction{basicInstruction{8, 0}, 3, b, regs},
+		0x99: &resetInstruction{basicInstruction{8, 0}, 3, c, regs},
+		0x9A: &resetInstruction{basicInstruction{8, 0}, 3, d, regs},
+		0x9B: &resetInstruction{basicInstruction{8, 0}, 3, e, regs},
+		0x9C: &resetInstruction{basicInstruction{8, 0}, 3, h, regs},
+		0x9D: &resetInstruction{basicInstruction{8, 0}, 3, l, regs},
+		0x9E: &resetFromMemoryInstruction{basicInstruction{16, 0}, 3, h, l, regs, mmu},
+		0x9F: &resetInstruction{basicInstruction{8, 0}, 3, a, regs},
+		0xA0: &resetInstruction{basicInstruction{8, 0}, 4, b, regs},
+		0xA1: &resetInstruction{basicInstruction{8, 0}, 4, c, regs},
+		0xA2: &resetInstruction{basicInstruction{8, 0}, 4, d, regs},
+		0xA3: &resetInstruction{basicInstruction{8, 0}, 4, e, regs},
+		0xA4: &resetInstruction{basicInstruction{8, 0}, 4, h, regs},
+		0xA5: &resetInstruction{basicInstruction{8, 0}, 4, l, regs},
+		0xA6: &resetFromMemoryInstruction{basicInstruction{16, 0}, 4, h, l, regs, mmu},
+		0xA7: &resetInstruction{basicInstruction{8, 1}, 4, a, regs},
+		0xA8: &resetInstruction{basicInstruction{8, 0}, 5, b, regs},
+		0xA9: &resetInstruction{basicInstruction{8, 0}, 5, c, regs},
+		0xAA: &resetInstruction{basicInstruction{8, 0}, 5, d, regs},
+		0xAB: &resetInstruction{basicInstruction{8, 0}, 5, e, regs},
+		0xAC: &resetInstruction{basicInstruction{8, 0}, 5, h, regs},
+		0xAD: &resetInstruction{basicInstruction{8, 0}, 5, l, regs},
+		0xAE: &resetFromMemoryInstruction{basicInstruction{16, 0}, 5, h, l, regs, mmu},
+		0xAF: &resetInstruction{basicInstruction{8, 0}, 5, a, regs},
+		0xB0: &resetInstruction{basicInstruction{8, 0}, 6, b, regs},
+		0xB1: &resetInstruction{basicInstruction{8, 0}, 6, c, regs},
+		0xB2: &resetInstruction{basicInstruction{8, 0}, 6, d, regs},
+		0xB3: &resetInstruction{basicInstruction{8, 0}, 6, e, regs},
+		0xB4: &resetInstruction{basicInstruction{8, 0}, 6, h, regs},
+		0xB5: &resetInstruction{basicInstruction{8, 0}, 6, l, regs},
+		0xB6: &resetFromMemoryInstruction{basicInstruction{16, 0}, 6, h, l, regs, mmu},
+		0xB7: &resetInstruction{basicInstruction{8, 1}, 6, a, regs},
+		0xB8: &resetInstruction{basicInstruction{8, 0}, 7, b, regs},
+		0xB9: &resetInstruction{basicInstruction{8, 0}, 7, c, regs},
+		0xBA: &resetInstruction{basicInstruction{8, 0}, 7, d, regs},
+		0xBB: &resetInstruction{basicInstruction{8, 0}, 7, e, regs},
+		0xBC: &resetInstruction{basicInstruction{8, 0}, 7, h, regs},
+		0xBD: &resetInstruction{basicInstruction{8, 0}, 7, l, regs},
+		0xBE: &resetFromMemoryInstruction{basicInstruction{16, 0}, 7, h, l, regs, mmu},
+		0xBF: &resetInstruction{basicInstruction{8, 0}, 7, a, regs},
+		0xC0: &setInstruction{basicInstruction{8, 0}, 0, b, regs},
+		0xC1: &setInstruction{basicInstruction{8, 0}, 0, c, regs},
+		0xC2: &setInstruction{basicInstruction{8, 0}, 0, d, regs},
+		0xC3: &setInstruction{basicInstruction{8, 0}, 0, e, regs},
+		0xC4: &setInstruction{basicInstruction{8, 0}, 0, h, regs},
+		0xC5: &setInstruction{basicInstruction{8, 0}, 0, l, regs},
+		0xC6: &setFromMemoryInstruction{basicInstruction{16, 0}, 0, h, l, regs, mmu},
+		0xC7: &setInstruction{basicInstruction{8, 0}, 0, a, regs},
+		0xC8: &setInstruction{basicInstruction{8, 0}, 1, b, regs},
+		0xC9: &setInstruction{basicInstruction{8, 0}, 1, c, regs},
+		0xCA: &setInstruction{basicInstruction{8, 0}, 1, d, regs},
+		0xCB: &setInstruction{basicInstruction{8, 0}, 1, e, regs},
+		0xCC: &setInstruction{basicInstruction{8, 0}, 1, h, regs},
+		0xCD: &setInstruction{basicInstruction{8, 0}, 1, l, regs},
+		0xCE: &setFromMemoryInstruction{basicInstruction{16, 0}, 1, h, l, regs, mmu},
+		0xCF: &setInstruction{basicInstruction{8, 0}, 1, a, regs},
+		0xD0: &setInstruction{basicInstruction{8, 0}, 2, b, regs},
+		0xD1: &setInstruction{basicInstruction{8, 0}, 2, c, regs},
+		0xD2: &setInstruction{basicInstruction{8, 0}, 2, d, regs},
+		0xD3: &setInstruction{basicInstruction{8, 0}, 2, e, regs},
+		0xD4: &setInstruction{basicInstruction{8, 0}, 2, h, regs},
+		0xD5: &setInstruction{basicInstruction{8, 0}, 2, l, regs},
+		0xD6: &setFromMemoryInstruction{basicInstruction{16, 0}, 2, h, l, regs, mmu},
+		0xD7: &setInstruction{basicInstruction{8, 0}, 2, a, regs},
+		0xD8: &setInstruction{basicInstruction{8, 0}, 3, b, regs},
+		0xD9: &setInstruction{basicInstruction{8, 0}, 3, c, regs},
+		0xDA: &setInstruction{basicInstruction{8, 0}, 3, d, regs},
+		0xDB: &setInstruction{basicInstruction{8, 0}, 3, e, regs},
+		0xDC: &setInstruction{basicInstruction{8, 0}, 3, h, regs},
+		0xDD: &setInstruction{basicInstruction{8, 0}, 3, l, regs},
+		0xDE: &setFromMemoryInstruction{basicInstruction{16, 0}, 3, h, l, regs, mmu},
+		0xDF: &setInstruction{basicInstruction{8, 0}, 3, a, regs},
+		0xE0: &setInstruction{basicInstruction{8, 0}, 4, b, regs},
+		0xE1: &setInstruction{basicInstruction{8, 0}, 4, c, regs},
+		0xE2: &setInstruction{basicInstruction{8, 0}, 4, d, regs},
+		0xE3: &setInstruction{basicInstruction{8, 0}, 4, e, regs},
+		0xE4: &setInstruction{basicInstruction{8, 0}, 4, h, regs},
+		0xE5: &setInstruction{basicInstruction{8, 0}, 4, l, regs},
+		0xE6: &setFromMemoryInstruction{basicInstruction{16, 0}, 4, h, l, regs, mmu},
+		0xE7: &setInstruction{basicInstruction{8, 0}, 4, a, regs},
+		0xE8: &setInstruction{basicInstruction{8, 0}, 5, b, regs},
+		0xE9: &setInstruction{basicInstruction{8, 0}, 5, c, regs},
+		0xEA: &setInstruction{basicInstruction{8, 0}, 5, d, regs},
+		0xEB: &setInstruction{basicInstruction{8, 0}, 5, e, regs},
+		0xEC: &setInstruction{basicInstruction{8, 0}, 5, h, regs},
+		0xED: &setInstruction{basicInstruction{8, 0}, 5, l, regs},
+		0xEE: &setFromMemoryInstruction{basicInstruction{16, 0}, 5, h, l, regs, mmu},
+		0xEF: &setInstruction{basicInstruction{8, 0}, 5, a, regs},
+		0xF0: &setInstruction{basicInstruction{8, 0}, 6, b, regs},
+		0xF1: &setInstruction{basicInstruction{8, 0}, 6, c, regs},
+		0xF2: &setInstruction{basicInstruction{8, 0}, 6, d, regs},
+		0xF3: &setInstruction{basicInstruction{8, 0}, 6, e, regs},
+		0xF4: &setInstruction{basicInstruction{8, 0}, 6, h, regs},
+		0xF5: &setInstruction{basicInstruction{8, 0}, 6, l, regs},
+		0xF6: &setFromMemoryInstruction{basicInstruction{16, 0}, 6, h, l, regs, mmu},
+		0xF7: &setInstruction{basicInstruction{8, 0}, 6, a, regs},
+		0xF8: &setInstruction{basicInstruction{8, 0}, 7, b, regs},
+		0xF9: &setInstruction{basicInstruction{8, 0}, 7, c, regs},
+		0xFA: &setInstruction{basicInstruction{8, 0}, 7, d, regs},
+		0xFB: &setInstruction{basicInstruction{8, 0}, 7, e, regs},
+		0xFC: &setInstruction{basicInstruction{8, 0}, 7, h, regs},
+		0xFD: &setInstruction{basicInstruction{8, 0}, 7, l, regs},
+		0xFE: &setFromMemoryInstruction{basicInstruction{16, 0}, 7, h, l, regs, mmu},
+		0xFF: &setInstruction{basicInstruction{8, 0}, 7, a, regs},
 	}
 }
 
@@ -673,13 +789,13 @@ func (i *bitFromMemoryInstruction) Execute(params Parameters) Addresser {
 }
 
 func (i *setInstruction) Execute(params Parameters) Addresser {
-	bit := byte(1 >> params[0])
+	bit := byte(1 >> i.bitNumber)
 	i.regs.WriteRegister(i.source, (i.regs.ReadRegister(i.source) | bit))
 	return &address{}
 }
 
 func (i *setFromMemoryInstruction) Execute(params Parameters) Addresser {
-	bit := byte(1 >> params[0])
+	bit := byte(1 >> i.bitNumber)
 	addr, err := i.regs.ReadRegisterPair(i.source1, i.source2)
 	if err != nil {
 		fmt.Println(err)
@@ -690,14 +806,14 @@ func (i *setFromMemoryInstruction) Execute(params Parameters) Addresser {
 }
 
 func (i *resetInstruction) Execute(params Parameters) Addresser {
-	bit := byte(1>>params[0]) ^ 0xFF
+	bit := byte(1>>i.bitNumber) ^ 0xFF
 	i.regs.WriteRegister(i.source, (i.regs.ReadRegister(i.source) & bit))
 	return &address{}
 	return &address{}
 }
 
 func (i *resetFromMemoryInstruction) Execute(params Parameters) Addresser {
-	bit := byte(1>>params[0]) ^ 0xFF
+	bit := byte(1>>i.bitNumber) ^ 0xFF
 	addr, err := i.regs.ReadRegisterPair(i.source1, i.source2)
 	if err != nil {
 		fmt.Println(err)
