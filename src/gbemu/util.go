60d0ab084c35d438371207343b610b452b089147
Mike <michael.barton0924@gmail.com>
2018-11-17 11:47:58 -0600

    Rename params in BitsToNum to be more clear

--- src/gbemu/util.go
+++ src/gbemu/util.go
@@ -8,12 +8,12 @@ func GetBitUint16(value uint16, bit uint) int {
 	return int((value >> bit) & 1)
 }
 
-func BitsToNum(highBit, lowBit int) int {
-	if lowBit == 0 && highBit == 0 {
+func BitsToNum(mostSignificantBit, leastSignificantBit int) int {
+	if leastSignificantBit == 0 && mostSignificantBit == 0 {
 		return 0
-	} else if lowBit == 1 && highBit == 0 {
+	} else if leastSignificantBit == 1 && mostSignificantBit == 0 {
 		return 1
-	} else if lowBit == 0 && highBit == 1 {
+	} else if leastSignificantBit == 0 && mostSignificantBit == 1 {
 		return 2
 	} else {
 		return 3
