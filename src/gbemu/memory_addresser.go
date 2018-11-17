a60e2a823152a531d3f36bb10beaec3bac532b8b
Mike <michael.barton0924@gmail.com>
2018-11-17 11:51:43 -0600

    Stop using unnecessary offset in memory addresser

--- src/gbemu/memory_addresser.go
+++ src/gbemu/memory_addresser.go
@@ -23,12 +23,12 @@ func CreateMemoryAddresser(addressMode AddressMode) MemoryAddresser {
 
 func (a *memoryAddresser) GetAddress(offset uint8) uint16 {
 	if a.addressMode == ADDRESS_MODE_8000 {
-		return uint16(0x8000 + uint16(offset)<<4 + 0x000E)
+		return uint16(0x8000 + uint16(offset)<<4)
 	} else {
 		if offset < 128 {
-			return uint16(0x9000 + uint16(offset)<<4 + 0x000E)
+			return uint16(0x9000 + uint16(offset)<<4)
 		} else {
-			return uint16(0x8800 + uint16(offset)<<4 + 0x000E)
+			return uint16(0x8800 + uint16(offset)<<4)
 		}
 	}
 }
